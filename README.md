56609485d6c9cb322e2bae2f5a15a89e71e4ca6e
luokui <luokui@magfin.cn>
2019-05-29 16:36:23 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# flask-docker-boilerplate
+Boilerplate for Flask applications with HAProxy using Docker
+## Usage
+```bash
+$ docker-compose up -d --no-recreate --scale app=5
+```
---
+++ app/views.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello from FLASK. My Hostname is: %s \n" % (socket.gethostname())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
