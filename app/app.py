bc03ab9f2c62c798a68452032a995263d984405a
luokui <luokui@magfin.cn>
2019-05-31 12:58:57 +0800

    refactor: 日志配置根据环境变化

--- app/app.py
+++ app/app.py
@@ -1,4 +1,18 @@
-from views import app
+import socket
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def dispatch():
+    '''
+    应用的统一入口，获取数据分发给不通的数据映射和决策，然后返回结果
+    :return:
+    '''
+    return "Hello from FLASK. My Hostname is: %s \n" % (socket.gethostname())
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
