69d39c825924f0493fb9090d222c295a30a977b0
luokui <luokui@magfin.cn>
2019-06-03 21:00:31 +0800

    refine code.

--- app/mapping/mysql_reader.py
+++ app/mapping/mysql_reader.py
@@ -3,9 +3,9 @@ from sqlalchemy import create_engine
 
 from config import GEARS_DB
 
-__DB_URI = 'mysql+pymysql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % GEARS_DB
+DB_URI = 'mysql+pymysql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % GEARS_DB
 
-__DB_ENGINE = create_engine(__DB_URI, encoding="utf8")
+__DB_ENGINE = create_engine(DB_URI, encoding="utf8")
 
 
 def sql_to_df(sql, index_col=None, coerce_float=True, params=None,
