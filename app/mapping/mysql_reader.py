5123118939f1e567cc04023ff7046c87b81ca39e
luokui <luokui@magfin.cn>
2019-06-05 14:22:30 +0800

    refine code.

--- app/mapping/mysql_reader.py
+++ app/mapping/mysql_reader.py
@@ -10,5 +10,8 @@ __DB_ENGINE = create_engine(DB_URI, encoding="utf8")
 
 def sql_to_df(sql, index_col=None, coerce_float=True, params=None,
               parse_dates=None, columns=None, chunksize=None):
-    return pd.read_sql(sql, __DB_ENGINE, index_col=None, coerce_float=True, params=None,
+    connect = __DB_ENGINE.connect()
+    df = pd.read_sql(sql, con=connect, index_col=None, coerce_float=True, params=None,
                        parse_dates=None, columns=None, chunksize=None)
+    connect.close()
+    return df
