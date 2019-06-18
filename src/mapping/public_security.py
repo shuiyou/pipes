1e168139f7bf529bdb5c7416bb59250261cc5d51
luokui <luokui@magfin.cn>
2019-06-18 18:28:51 +0800

    refactor: 重构公安

--- src/mapping/public_security.py
+++ src/mapping/public_security.py
@@ -1,21 +1,31 @@
 from mapping.mysql_reader import sql_to_df
 
 
-def ps_name_id(user_name, id_card_no):
+class PublicSecurity:
     """
-    判断用户名和证件号是否匹配
-    :param user_name:
-    :param id_card_no:
-    :return: 如果匹配返回 1， 不匹配返回 0
+    公安相关的变量模块
     """
-    sql_str = '''
-        SELECT user_name, id_card_no, result FROM info_certification 
-        WHERE certification_type = 'ID_NAME' AND unix_timestamp(NOW()) < unix_timestamp(expired_at)
-        AND user_name = %(user_name)s AND id_card_no = %(id_card_no)s
-        ORDER BY expired_at DESC LIMIT 1;
-    '''
-    df = sql_to_df(sql=(sql_str), params={"user_name": user_name, "id_card_no": id_card_no})
-    result = 0
-    if len(df) == 1 and df['result'][0] == b'\x01':
-        result = 1
-    return result
+
+    def __init__(self, user_name, id_card_no) -> None:
+        super().__init__()
+        self.user_name = user_name
+        self.id_card_no = id_card_no
+
+    def ps_name_id(self):
+        """
+        判断用户名和证件号是否匹配
+        :param user_name:
+        :param id_card_no:
+        :return: 如果匹配返回 1， 不匹配返回 0
+        """
+        sql_str = '''
+            SELECT user_name, id_card_no, result FROM info_certification 
+            WHERE certification_type = 'ID_NAME' AND unix_timestamp(NOW()) < unix_timestamp(expired_at)
+            AND user_name = %(user_name)s AND id_card_no = %(id_card_no)s
+            ORDER BY expired_at DESC LIMIT 1;
+        '''
+        df = sql_to_df(sql=(sql_str), params={"user_name": self.user_name, "id_card_no": self.id_card_no})
+        result = 0
+        if len(df) == 1 and df['result'][0] == b'\x01':
+            result = 1
+        return result
--- tests/test_public_security.py
+++ tests/test_public_security.py
@@ -1,7 +1,8 @@
-from mapping.public_security import ps_name_id
+from mapping.public_security import PublicSecurity
 
 
 def test_ps_name_id():
-    result = ps_name_id(user_name='任震东', id_card_no='150304197609302532')
+    ps = PublicSecurity(user_name='任震东', id_card_no='150304197609302532')
+    result = ps.ps_name_id()
     print("result is" + str(result))
     assert result == 0
