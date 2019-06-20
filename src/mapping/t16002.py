edcee5a610eb1681228c8482e6ae8f571611981c
luokui <luokui@magfin.cn>
2019-06-20 19:17:44 +0800

    添加缺失的转换

---
+++ src/mapping/t16002.py
@@ -0,0 +1,10 @@
+from mapping.tranformer import Transformer
+
+
+class T16002(Transformer):
+    """
+    法院核查 企业
+    """
+
+    def transform(self, user_name=None, id_card_no=None, phone=None):
+        pass
