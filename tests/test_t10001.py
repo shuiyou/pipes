f06c29072518468ec9d5791a363d3b928139ceef
luokui <luokui@magfin.cn>
2019-06-25 12:00:59 +0800

    添加时间比较方法

--- tests/test_t10001.py
+++ tests/test_t10001.py
@@ -1,5 +1,11 @@
-from mapping import t10001
+import pandas as pd
+
+from mapping.t10001 import T10001
 
 
 def test__ovdu_sco_1y():
-    t10001.input()
+    t10001 = T10001()
+    t10001.input('111', '1111', '111')
+    mock_df = pd.read_csv('data/info_risk_overdue.csv', parse_dates=True)
+    t10001._subtract_by_year(mock_df)
+    t10001._ovdu_sco_1y(df=mock_df)
