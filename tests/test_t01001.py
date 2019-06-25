b72c76e9f2ee414840fbaf199df56dbfed9a5d4d
luokui <luokui@magfin.cn>
2019-06-25 15:56:44 +0800

    phone_on_line_state

---
+++ tests/test_t01001.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+from mapping.t01001 import T01001
+from mapping.t02001 import T02001
+
+
+def test_phone_on_line_days():
+    t = T01001()
+    t.run(user_name='刘劭卓', id_card_no='430105199106096118', phone='11111111111')
+    print(t.variables)
+
+    t._phone_on_line_state(df=pd.DataFrame({
+        'mobile_state': ['NORMAL']
+    }))
+    print(t.variables)
+    assert t.variables['phone_on_line_state'] == 'NORMAL'
