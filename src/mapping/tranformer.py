eb4580a5f754817b4067934ed134ae687a2e6968
luokui <luokui@magfin.cn>
2019-06-25 11:21:09 +0800

    添加时间比较方法

--- src/mapping/tranformer.py
+++ src/mapping/tranformer.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from abc import ABCMeta, abstractmethod
 
+import numpy as np
+
 
 class Transformer(object):
     __metaclass__ = ABCMeta
@@ -29,3 +31,22 @@ class Transformer(object):
         :return:
         """
         pass
+
+
+    def subtract_datetime_col(self, df, col_name1, col_name2, time_unit='M'):
+        """
+        计算两列时间差
+        :param df:
+        :param col_name1:
+        :param col_name2:
+        :param time_unit: 时间差的单位 D 天, W 周, M 月, Y 年
+        :return:
+        """
+        cols = df.columns
+        if col_name1 in cols and col_name2 in cols:
+            sub_name = col_name1 + '-' + col_name1 + '-year'
+            df[sub_name] = df[col_name1] - df[col_name2]
+            df[sub_name] = df[sub_name] / np.timedelta64(1, time_unit)
+            return sub_name
+        else:
+            return None
