fc6776303b97813558bf2363c6a8a0b89cb6f52f
gulongwei <gulongwei@magfin.cn>
2019-06-25 09:59:31 +0800

    反欺诈

--- tests/metabase/test_t12001.py
+++ tests/metabase/test_t12001.py
@@ -13,7 +13,4 @@ def test_t12001():
     ps3.transform('莫四', '450404198601010031','13722345670')
     assert ps3.variables == {'anti_idno_consume_black': 0, 'anti_idno_P2P_black': 0, 'anti_idno_lost_black': 0, 'anti_idno_fraud_black': 0, 'anti_idno_loan_black': 0, 'anti_tel_industy_black': 0, 'anti_tel_P2P_black': 0, 'anti_tel_lost_black': 0, 'anti_tel_fraud_black': 0, 'anti_tel_small_no': 0, 'anti_idno_court_execu': 0, 'anti_idno_court_closure': 0, 'anti_idno_court_break_faith': 0, 'anti_idno_legal_break_faith': 0, 'anti_idno_apply_1m': 0, 'anti_idno_apply_3m': 0, 'anti_tel_apply_1m': 0, 'anti_tel_apply_3m': 0, 'anti_idno_apply_3d': 0, 'anti_idno_apply_7d': 0, 'anti_tel_apply_3d': 0, 'anti_tel_apply_7d': 0}
 
-    ps4 = T12001()
-    ps4.transform('云高琼', '422826198009300047','15347020567')
-    assert ps4.variables == {'anti_idno_consume_black': 0, 'anti_idno_P2P_black': 0, 'anti_idno_lost_black': 0, 'anti_idno_fraud_black': 0, 'anti_idno_loan_black': 0, 'anti_tel_industy_black': 0, 'anti_tel_P2P_black': 0, 'anti_tel_lost_black': 0, 'anti_tel_fraud_black': 0, 'anti_tel_small_no': 0, 'anti_idno_court_execu': 0, 'anti_idno_court_closure': 0, 'anti_idno_court_break_faith': 0, 'anti_idno_legal_break_faith': 0, 'anti_idno_apply_1m': 0, 'anti_idno_apply_3m': 0, 'anti_tel_apply_1m': 0, 'anti_tel_apply_3m': 0, 'anti_idno_apply_3d': 0, 'anti_idno_apply_7d': 0, 'anti_tel_apply_3d': 0, 'anti_tel_apply_7d': 0}
 
