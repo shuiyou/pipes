25f22a7fe9cc9f6d7882b674b0cec4f38dbb60ff
luokui <luokui@magfin.cn>
2019-06-26 11:48:50 +0800

    实现18001

--- tests/test_t18001.py
+++ tests/test_t18001.py
@@ -1,7 +1,7 @@
 from mapping.t18001 import T18001
 
 
-def test_phone_on_line_days():
+def test_18001():
     t = T18001()
     t.run(user_name='段娜', id_card_no='411403198609101683')
     print(t.variables)
