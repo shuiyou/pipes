10114153e955b1d27ae3f7e81c4c519c52ef0d1b
gulongwei <gulongwei@magfin.cn>
2019-06-26 15:27:51 +0800

    逾期核查_字段更新

--- src/mapping/t10001.py
+++ src/mapping/t10001.py
@@ -1,5 +1,5 @@
 from mapping.mysql_reader import sql_to_df
-from mapping.tranformer import Transformer, subtract_datetime_col
+from mapping.tranformer import Transformer,subtract_datetime_col
 
 
 class T10001(Transformer):
@@ -10,7 +10,9 @@ class T10001(Transformer):
     def __init__(self) -> None:
         super().__init__()
         self.variables = {
-            'ovdu_sco_1y': 0
+            'ovdu_sco_1y': 0,
+            'ovdu_sco': 0,
+            'ovdu_sco_time': 0
         }
 
     def _info_risk_overdue_df(self):
@@ -20,24 +22,34 @@ class T10001(Transformer):
             AND risk_score is not NULL
             AND user_name = %(user_name)s AND id_card_no = %(id_card_no)s;
         """
-        df = sql_to_df(sql=info_certification,
-                       params={"user_name": self.user_name,
-                               "id_card_no": self.id_card_no})
-        self._subtract_by_year(df)
+        df = sql_to_df(sql=info_certification, params={"user_name": self.user_name, "id_card_no": self.id_card_no})
+        df['date_dif'] = df[subtract_datetime_col(df, 'create_time', 'data_build_time', 'Y')]
         return df
 
-    def _subtract_by_year(self, df):
-        self.diff_year = subtract_datetime_col(df, 'create_time', 'data_build_time', 'Y')
-
     def _ovdu_sco_1y(self, df=None):
-        """
-        逾期核查_近1年内最大风险得分
-        :param user_name:
-        :param id_card_no:
-        """
-        if df is not None and len(df) > 0:
-            df = df.query(self.diff_year + ' < 1')
-            self.variables['ovdu_sco_1y'] = df['risk_score'].max()
+        if len(df) > 0:
+            df1 = df[df['date_dif'] < 1]
+            if len(df1) > 0:
+                self.variables['ovdu_sco_1y'] = df1['risk_score'].max()
+
+    def _ovdu_sco(self, df=None):
+        if len(df) > 0:
+            self.variables['ovdu_sco'] = df['risk_score'].max()
+
+    def _ovdu_sco_time(self, df=None):
+        if len(df) > 0:
+            df1 = list(df.loc[df['risk_score'] == df['risk_score'].max(), 'date_dif'])
+            if df1[0] < 1:
+                self.variables['ovdu_sco_time'] = 1
+            elif df1[0] < 2:
+                self.variables['ovdu_sco_time'] = 2
+            elif df1[0] < 3:
+                self.variables['ovdu_sco_time'] = 3
+            else:
+                self.variables['ovdu_sco_time'] = 4
 
     def transform(self):
-        self._ovdu_sco_1y(self._info_risk_overdue_df())
+        info_risk_overdue_df = self._info_risk_overdue_df()
+        self._ovdu_sco_1y(info_risk_overdue_df)
+        self._ovdu_sco(info_risk_overdue_df)
+        self._ovdu_sco_time(info_risk_overdue_df)
