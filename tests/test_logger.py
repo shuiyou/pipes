30a517b8d98e10c88f20e9a21d3fad98fee74744
luokui <luokui@magfin.cn>
2019-06-28 09:58:38 +0800

    refine code.

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -2,4 +2,6 @@ from logger.logger_util import LoggerUtil
 
 
 def test_log_stash():
-    LoggerUtil()
+    logger_util = LoggerUtil()
+    logger = logger_util.logger(__name__)
+    logger.info("test my logger....")
