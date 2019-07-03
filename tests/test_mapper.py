02527fadbc11b175f3867b48a47cbcdcb1b1dcb5
liujinhao <liujinhao@magfin.cn>
2019-07-03 17:29:48 +0800

    /feat :  本地提交

--- tests/test_mapper.py
+++ tests/test_mapper.py
@@ -1,5 +1,4 @@
 from mapping.mapper import translate, get_transformer
-import pandas as pd
 
 
 def test_type():
@@ -10,11 +9,8 @@ def test_type():
 
 
 def test_transform():
-    codes=['16001']
-    res = translate(codes, user_name='昌鹏', id_card_no='540228195907066426')
-    # df = pd.DataFrame(res,pd.Index(range(1)))
-    # print(df['aaa'])
+    codes=['00000', '01001','02001','05001','05002','06001', '07001','08001',
+           '09001', '10001', '11001', '12001', '13001', '14001', '16001', '16002',
+           '17001', '18001', 'f0001', 'f0002', 'f0003']
+    res = translate(codes, user_name='张虎', id_card_no='430122197512087812')
     print(res)
-    for key in res:
-       if 'base_date'==key:
-           print(res[key])
