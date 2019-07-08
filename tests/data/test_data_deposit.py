f41d6d64ce6557134d0ef311bd1dc6cc28ac2832
liujinhao <liujinhao@magfin.cn>
2019-07-08 09:52:57 +0800

    /feat :  本地提交

--- tests/data/test_data_deposit.py
+++ tests/data/test_data_deposit.py
@@ -6,40 +6,40 @@ from data.data_union_multi_to_multi_deposit import unit_multi_deposit
 
 
 def test_01001_case():
-    _assert_df('01001')
+    _assert_single_df('01001')
 
 def test_02001_case():
-    _assert_df('02001')
+    _assert_single_df('02001')
 
 def test_05001_case():
-    _assert_df('05001')
+    _assert_single_df('05001')
 
 def test_05002_case():
-    _assert_df('05002')
+    _assert_single_df('05002')
 
 def test_06001_case():
-    _assert_df('06001')
+    _assert_single_df('06001')
 
 def test_08001_case():
-    _assert_df('08001')
+    _assert_single_df('08001')
 
 def test_09001_case():
-    _assert_df('09001')
+    _assert_single_df('09001')
 
 def test_11001_case():
-    _assert_df('11001')
+    _assert_single_df('11001')
 
 def test_13001_case():
-    _assert_df('13001')
+    _assert_single_df('13001')
 
 def test_14001_case():
-    _assert_df('14001')
+    _assert_single_df('14001')
 
 def test_16001_case():
-    _assert_df('16001')
+    _assert_single_df('16001')
 
 def test_16002_case():
-    _assert_df('16002')
+    _assert_single_df('16002')
 
 def test_f001_case():
     _assert_union_multi_to_multi_df('f001')
@@ -54,36 +54,25 @@ def test_f0003_case():
 
 def _assert_union_multi_to_multi_df(code):
     ps = unit_multi_deposit();
-    df = ps.run(read_file_name="一级测试用例-" + code + ".xlsx")
-    result_df = df[df['是否通过'] == 'false']
-    if len(result_df) > 0:
-        print("测试错了：" + code)
-    assert result_df.shape[0] == 0
+    _assert_df(ps, code)
 
 def _assert_union_multi_to_one_df(code):
     ps = unit_deposit();
-    df = ps.run(read_file_name="一级测试用例-" + code + ".xlsx")
-    result_df = df[df['是否通过'] == 'false']
-    if len(result_df) > 0:
-        print("测试错了：" + code)
-    assert result_df.shape[0] == 0
+    _assert_df(ps, code)
 
-
-
-def _assert_df(code):
+def _assert_single_df(code):
     ps = deposit()
+    _assert_df(ps,code)
+
+
+
+def _assert_df(ps,code):
     df = ps.run(read_file_name="一级测试用例-" + code + ".xlsx")
     result_df = df[df['是否通过'] == 'false']
     if len(result_df) > 0:
         print("测试错了：" + code)
     assert result_df.shape[0] == 0
 
-
-
-def test_num():
-    value = 'social_tel_gray_sco='
-    expect_result = value.split('=')[1]
-    print(expect_result)
 
 
 
