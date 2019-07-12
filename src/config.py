d9566a62907e9a38a82734d994a79be17c251606
luokui <luokui@magfin.cn>
2019-07-12 13:23:21 +0800

    refine db config.

--- src/config.py
+++ src/config.py
@@ -8,19 +8,11 @@ app_env = os.getenv("ENV", 'test').lower()
 STRATEGY_URL = os.getenv('STRATEGY_URL',
                          'http://192.168.1.20:8091/rest/S1Public')  ## 获取环境变量，如果没有返回'http://192.168.1.20:8091/rest/S1Public'
 
-# GEARS_DB = {
-#     'user': os.getenv('GEARS_USER', 'gears_dev'),
-#     'pw': os.getenv('GEARS_PW', 'x2cqpau4'),
-#     'host': os.getenv('GEARS_HOST', '192.168.1.9'),
-#     'port': os.getenv('GEARS_PORT', 3360),
-#     'db': os.getenv('DB_NAME', 'gears_dev'),
-# }
-
 # 通过环境变量配置不同部署环境的是数据库
 GEARS_DB = {
-    'user': os.getenv('DB_USER', 'gears_test'),
-    'pw': os.getenv('DB_PW', 'bd3by68u'),
-    'host': os.getenv('DB_HOST', '192.168.1.9'),
-    'port': os.getenv('DB_PORT', 3360),
-    'db': os.getenv('DB_NAME', 'gears_test'),
+    'user': os.getenv('GEARS_USER', 'gears_dev'),
+    'pw': os.getenv('GEARS_PW', 'x2cqpau4'),
+    'host': os.getenv('GEARS_HOST', '192.168.1.9'),
+    'port': os.getenv('GEARS_PORT', 3360),
+    'db': os.getenv('DB_NAME', 'gears_dev'),
 }
