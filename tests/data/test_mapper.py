f7b5e6e8e6357b1ce52cbb4dd04ddbe6246b058d
liujinhao <liujinhao@magfin.cn>
2019-07-16 14:36:59 +0800

    /feat :  本地提交

--- tests/data/test_mapper.py
+++ tests/data/test_mapper.py
@@ -1,30 +1,23 @@
 # -*- coding: utf-8 -*-
 import importlib
 
-# from app import logger
-import numpy
-
 from exceptions import ServerException
 from logger.logger_util import LoggerUtil
 from mapping.tranformer import Transformer, numpy_to_int
 
+# from app import logger
+
 logger = LoggerUtil().logger(__name__)
 
-# 标识决策引擎交互完成。
-STRATEGE_DONE = 'fffff'
 
-
-
-
-def translate_for_report_detail(codes, user_name=None, id_card_no=None, phone=None, user_type=None):
+def translate_for_strategy(codes, user_name=None, id_card_no=None, phone=None, user_type=None):
     """
     根据产品编码对应的excel文件从Gears数据库里获取数据做转换处理。
     处理后的结果作为决策需要的变量。
     :return: 一个dict对象包含产品所需要的变量
     """
-
     variables = {}
-
+    c = None
     try:
         for c in codes:
             trans = get_transformer(c)
@@ -34,7 +27,7 @@ def translate_for_report_detail(codes, user_name=None, id_card_no=None, phone=No
                                      user_type=user_type)
             variables.update(trans_result['variables'])
     except Exception as err:
-        logger.error(">>> translate error: " + str(err))
+        logger.error(c + ">>> translate error: " + str(err))
         raise ServerException(code=500, description=str(err))
     # 转换类型，这样解决tojson的问题
     numpy_to_int(variables)
@@ -48,8 +41,8 @@ def get_transformer(code) -> Transformer:
     :return:
     """
     try:
-        model = importlib.import_module("view.v" + str(code))
-        api_class = getattr(model, "V" + str(code))
+        model = importlib.import_module("mapping.t" + str(code))
+        api_class = getattr(model, "T" + str(code))
         api_instance = api_class()
         return api_instance
     except ModuleNotFoundError as err:
