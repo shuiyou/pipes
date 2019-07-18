e933830db5c1e0d815855ebd3b57e998f89738d6
luokui <luokui@magfin.cn>
2019-07-18 14:46:59 +0800

    fix unit test。

--- tests/mapping/test_mapper.py
+++ tests/mapping/test_mapper.py
@@ -5,7 +5,7 @@ def test_type():
     t05002 = get_transformer('05002')
     t05002.run(user_name='张虎', id_card_no='430122197512087812')
     print(t05002.variables)
-    assert t05002.variables['ps_name_id'] == 0
+    assert t05002.variables['ps_name_id'] == 1
 
 
 def test_transform():
