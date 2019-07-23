fdb1168cd4f34426a2cd6c2e7de44c528be77810
Allen <zhaipengmath@126.com>
2019-07-23 15:45:56 +0800

    f0004测试完成

--- tests/mapping/test_tf0004.py
+++ tests/mapping/test_tf0004.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def test_ps_court_info():
     ps = Tf0004()
-    ps.run(user_name='尤淑华')
+    ps.run(user_name='罗淑英')
     ent_on_status = ['在营（开业）', '存续（在营、开业、在册）']
     legal_df = ps._com_bus_frinv_df(status=ent_on_status)
     shareholder_df = ps._com_bus_entinvitem_df(status=ent_on_status)
@@ -12,5 +12,9 @@ def test_ps_court_info():
     court_merge_df = ps._court_info_df(df=concat_df)
     judicative_df = ps._court_judicative_pape_df(df=court_merge_df)
     trial_df = ps._court_trial_process_df(df=court_merge_df)
+    court_merge_df = ps._court_info_df(df=concat_df)
     dispute_df = pd.concat([judicative_df, trial_df])
-    print(dispute_df)
+
+
+    print(ps.variables['com_bus_court_open_docu_status'])
+
