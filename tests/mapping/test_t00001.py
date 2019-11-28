3fd8f6d8622c18607f2aa09c67343855e4aff7a5
lixiaobo <lixiaobo@magfin.cn>
2019-11-28 15:44:22 +0800

    refine pytest for T00001.py

--- tests/mapping/test_t00001.py
+++ tests/mapping/test_t00001.py
@@ -10,10 +10,10 @@ from util.defensor_client import DefensorClient
 
 def test_df_client():
     df_client = DefensorClient(None)
-    df_client.app_id = "9999999999"
+    df_client.app_id = "99999"
     df_client.grey_list_query_url = "http://192.168.1.15:100/gateway/defensor/api/open/grey-list/hit"
 
     t = T00001()
     t.df_client = df_client
-    t.run(user_name='孙洋洋', id_card_no='340321198704272796', phone='11111111111')
+    t.run(user_name='蒋育宣', id_card_no='321088196405036526', phone='11111111111')
     print("-----", t.variables)
