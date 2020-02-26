866ea88cbc579ce158df83c9573a846bfd9d0dbd
lixiaobo <lixiaobo@magfin.cn>
2020-02-26 10:48:56 +0800

    refine df_compare function

--- src/mapping/utils/df_comparator_util.py
+++ src/mapping/utils/df_comparator_util.py
@@ -4,14 +4,15 @@ from util.mysql_reader import sql_to_df
 
 
 def df_compare(variables, old_df, new_df, variable_name):
-    if (old_df.shape[0] == 0 and new_df.shape[0] == 0) or new_df.shape[0] == 0:
+    if old_df.shape[0] == 0 and new_df.shape[0] == 0:
         return
-
-    find = True
-    for index, row in new_df.iterrows():
-        find &= _find_value(old_df, row[0])
-
-    variables[variable_name] = 0 if find else 1
+    elif new_df.shape[0] == 0:
+        variables[variable_name] = 0
+    elif old_df.shape[0] == 0:
+        variables[variable_name] = 1
+    else:
+        diff_set = set(new_df.iloc[:, 0]).difference(set(old_df.iloc[:, 0]))
+        variables[variable_name] = 0 if len(diff_set) == 0 else 1
 
 
 def _find_value(df, val):
