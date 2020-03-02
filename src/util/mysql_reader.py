c76a4923b903e456025aa717a726508a19e35133
lixiaobo <lixiaobo@magfin.cn>
2020-03-02 10:37:44 +0800

    sqlalchemy pool size setting

--- src/util/mysql_reader.py
+++ src/util/mysql_reader.py
@@ -7,7 +7,7 @@ from config import GEARS_DB
 
 DB_URI = 'mysql+pymysql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % GEARS_DB
 
-__DB_ENGINE = create_engine(DB_URI, encoding="utf8")
+__DB_ENGINE = create_engine(DB_URI, pool_size=10, max_overflow=10, encoding="utf8")
 
 
 def sql_to_df(sql, index_col=None, coerce_float=True, params=None,
