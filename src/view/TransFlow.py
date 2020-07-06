b621e596306367e145638cec0f982b2f5a289fc7
黎小波 <lixiaobo@magfin.cn>
2020-07-06 10:53:36 +0800

    Revert "Merge branch 'feat_流水报告_lsq' into 'feat_流水报告'"
    This reverts merge request !108

--- src/view/TransFlow.py
+++
@@ -1,8 +0,0 @@
-from mapping.module_processor import ModuleProcessor
-
-class TransFlow(ModuleProcessor):
-
-    def __init__(self):
-        super().__init__()
-        # self.db = self._db()
-        self.variables = {}
