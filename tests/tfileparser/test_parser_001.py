6e16f472231790201b7c4d89d620cb6d74e92be1
pythonlearner3608 <3608380265@qq.com>
2020-07-20 09:11:07 +0800

    20200720_v1

--- tests/tfileparser/test_parser_001.py
+++ tests/tfileparser/test_parser_001.py
@@ -2,7 +2,25 @@ from app import sql_db
 from fileparser.Parser001 import Parser001
 
 
-file = r"../resource/trans_flow/账号6226190200138646交易明细070902.xls"
+file_0718_list = [
+    r"../resource/trans_flow/0718文件类型错误校验失败.xls",
+    r"../resource/trans_flow/0718空文件校验失败.xlsx",
+    r"../resource/trans_flow/0718姓名不匹配校验失败.xlsx",
+    r"../resource/trans_flow/0718账户不匹配校验失败.xlsx",
+    r"../resource/trans_flow/0718时间格式不符合校验失败.xlsx"
+    r"../resource/trans_flow/0718删除一行数据验真失败.xlsx",
+    r"../resource/trans_flow/0718修改一个时间验真失败.xlsx"
+]
+param_0718 = {
+    'cusType': "PERSONAL",
+    'cusName': "张三",
+    'idNo': "202202198604064532",
+    'idType': "ID_CARD_NO",
+    'bankAccount': "6222620110014694302",
+    'bankName': "xx银行",
+}
+
+file = r"../resource/trans_flow/1595235951.xlsx"
 param = {
     'bankAccount': '6226190200138646',
     'cusName': '赵高枫',
@@ -91,5 +109,15 @@ def test_003():
     for i in range(4):
         p.file = file_list[i]
         p.param = param_list[i]
+        resp = p.process()
+        print(resp)
+
+
+def test_004():
+    p = Parser001()
+    p.sql_db = sql_db()
+    for i in range(len(file_0718_list)):
+        p.file = file_0718_list[i]
+        p.param = param_0718
         resp = p.process()
         print(resp)
