5bf1521f7024b39186cb7342a15a5e0b948aad98
lixiaobo <lixiaobo@magfin.cn>
2020-07-24 10:34:46 +0800

    fix: 时间偏移

--- src/view/p08001_v/json_u_portrait.py
+++ src/view/p08001_v/json_u_portrait.py
@@ -1,5 +1,7 @@
 import json
 
+import pandas as pd
+
 from view.TransFlow import TransFlow
 from util.mysql_reader import sql_to_df
 
@@ -17,10 +19,21 @@ class JsonUnionPortrait(TransFlow):
         """
         df = sql_to_df(sql=sql,
                        params={"report_req_no": self.reqno})
-        df.drop(columns = ['id','apply_no','report_req_no','create_time','update_time'],inplace = True)
-
+        df.drop(columns = ['id','apply_no','create_time','update_time'],inplace = True)
         df['analyse_start_time'] = df['analyse_start_time'].astype(str)
         df['analyse_end_time'] = df['analyse_end_time'].astype(str)
+
+        sql1 = '''
+            select report_req_no,min(trans_date) as loan_start_time
+            from trans_u_flow_portrait
+            where report_req_no = %(report_req_no)s
+            '''
+        df1 = sql_to_df(sql=sql1,
+                       params={"report_req_no": self.reqno})
+
+        df1['loan_start_time'] = df1['loan_start_time'].astype(str)
+
+        df = pd.merge(df,df1,how='left', on='report_req_no' ).drop(columns = 'report_req_no')
 
         self.variables["trans_u_portrait"] = {}
 
