ea28914457b29ec76087d2c8d2c25d9fc0a20f85
liujinhao <liujinhao@magfin.cn>
2020-10-13 15:01:07 +0800

    白骑士

---
+++ src/mapping/t35001.py
@@ -0,0 +1,50 @@
+from mapping.tranformer import Transformer
+from util.mysql_reader import sql_to_df
+
+
+def get_field_value(df, key):
+    df_temp = df[df['field_name'] == key]
+    if not df_temp.empty:
+        return float(df_temp['field_value'].to_list()[0])
+    else:
+        return 0
+
+
+class T35001(Transformer):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.variables = {
+            'FIN_Loan_all_0M_1M': 0,
+            'FIN_Debit_PCT_all_1M_2M': 0,
+            'FIN_Loan_Small_uninstall_0M_1M': 0,
+            'FIN_Loan_Small_all_0M_1M': 0,
+            'FIN_Loan_P2P_uninstall_0M_1M': 0,
+            'FIN_Loan_P2P_all_0M_1M': 0
+        }
+
+    def _info_risk_factor_item(self):
+        sql = '''
+            select field_name,field_value from info_risk_factor_item where risk_factor_id = (
+                select id from info_risk_factor where mobile = %(mobile)s 
+                and unix_timestamp(NOW()) < unix_timestamp(expired_at) order by id desc limit 1
+            )
+        '''
+        df = sql_to_df(sql=sql,
+                       params={"mobile": self.phone})
+        return df
+
+    def clean_variables(self):
+        df = self._info_risk_factor_item()
+        if df.empty:
+            return
+        self.variables['FIN_Loan_uninstall_0M_1M'] = get_field_value(df, 'FIN_Loan_uninstall_0M_1M')
+        self.variables['FIN_Loan_all_0M_1M'] = get_field_value(df, 'FIN_Loan_all_0M_1M')
+        self.variables['FIN_Debit_PCT_all_1M_2M'] = get_field_value(df, 'FIN_Debit_PCT_all_1M_2M')
+        self.variables['FIN_Loan_Small_uninstall_0M_1M'] = get_field_value(df, 'FIN_Loan_Small_uninstall_0M_1M')
+        self.variables['FIN_Loan_Small_all_0M_1M'] = get_field_value(df, 'FIN_Loan_Small_all_0M_1M')
+        self.variables['FIN_Loan_P2P_uninstall_0M_1M'] = get_field_value(df, 'FIN_Loan_P2P_uninstall_0M_1M')
+        self.variables['FIN_Loan_P2P_all_0M_1M'] = get_field_value(df, 'FIN_Loan_P2P_all_0M_1M')
+
+    def transform(self):
+        self.clean_variables()
--- src/mapping/t61002.py
+++ src/mapping/t61002.py
@@ -1,5 +1,13 @@
 from mapping.tranformer import Transformer
+from util.mysql_reader import sql_to_df
 
+
+def check(df, key1, key2):
+    df_temp = df[(df['field_name'] == key1) & (df['field_value'] == key2)]
+    if not df_temp.empty:
+        return 1
+    else:
+        return 0
 
 class T61002(Transformer):
     """
@@ -9,15 +17,40 @@ class T61002(Transformer):
     def __init__(self) -> None:
         super().__init__()
         self.variables = {
-            'i_cnt_partner_Loan_finance_180day': 0,
-            'i_cnt_partner_all_Imbank_90day': 0,
-            'i_std_cnt_10daypartner_all_Imbank_90day': 0,
-            'm_cnt_partner_all_finance_180day': 0,
-            'm_cnt_partner_all_Imbank_90day': 0,
-            'i_cnt_partner_all_P2pweb_90day': 0,
-            'i_cnt_partner_all_Imbank_180day': None,
-            'm_max_cnt_partner_daily_all_Unconsumerfinance_365day': None,
-            'm_ratio_cnt_grp_id_Loan_all_all': None,
-            'm_length_first_last_all_Imbank_60day': None,
-            'i_length_first_last_all_Imbank_365day': None
+            'bqs_risk_cert_cnt': 0,
+            'bqs_risk_tele_cnt': 0,
+            'bqs_net_tele_cnt': 0,
+            'bqs_net_cert_cnt': 0,
+            'bqs_cross_event_risk': 0,
+            'bqs_cross_equip_risk': 0,
+            'bqs_model_middle_risk': 0,
+            'bqs_model_high_risk': 0
         }
+
+    def _info_stats_item(self):
+        sql = '''
+            select field_name,field_value from info_stats_item where stats_id = (
+                select id from info_stats where user_name = %(user_name)s and id_card_no = %(id_card_no)s
+                and mobile_hit_type = '1' or cert_no_hit_type = '1'
+                and unix_timestamp(NOW()) < unix_timestamp(expired_at) order by id desc limit 1 
+            )
+        '''
+        df = sql_to_df(sql=sql,
+                       params={"user_name": self.user_name,
+                               "id_card_no": self.id_card_no})
+        return df
+
+    def clean_variables(self):
+        df = self._info_stats_item()
+        self.variables['bqs_risk_cert_cnt'] = check(df, 'secondType', '13001')
+        self.variables['bqs_risk_tele_cnt'] = check(df, 'secondType', '13002')
+        self.variables['bqs_net_tele_cnt'] = check(df, 'secondType', '32001')
+        self.variables['bqs_net_cert_cnt'] = check(df, 'secondType', '32002')
+        self.variables['bqs_cross_event_risk'] = check(df, 'secondType', '60001')
+        self.variables['bqs_cross_equip_risk'] = check(df, 'secondType', '60002')
+        self.variables['bqs_model_middle_risk'] = check(df, 'secondType', '61001')
+        self.variables['bqs_model_high_risk'] = check(df, 'secondType', '61002')
+
+
+    def transform(self):
+        self.clean_variables()
