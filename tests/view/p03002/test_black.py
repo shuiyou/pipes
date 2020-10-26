e86e87cfbdccb31f61b30920e01498471205bcaf
liujinhao <liujinhao@magfin.cn>
2020-10-26 15:40:45 +0800

    live_address  长度修改

--- tests/view/p03002/test_black.py
+++ tests/view/p03002/test_black.py
@@ -1,7 +1,12 @@
-from view.p03002.black import Black
+import json
+
+from file_utils.files import file_content
+
+from view.p03002.black_union import Black
 
 
 def test_001():
     ps = Black()
+    ps.full_msg = json.loads(file_content(r"C:/workspace/pipes/tests/resource", "unin_level1_001.json"))
     ps.run()
     print(ps.variables)
