d512a41f2cbdcc72d636f3d4694e434c037e8efc
lishiquan@magfin.cn <lsq147LSQ>
2020-11-24 17:45:46 +0800

    11-24: 开发记录2

--- src/view/p07003/unsettled_pie.py
+++ src/view/p07003/unsettled_pie.py
@@ -1,5 +1,6 @@
 from mapping.grouped_tranformer import GroupedTransformer, invoke_each
-
+import pandas as pd
+import numpy as np
 
 class UnsettledPie(GroupedTransformer):
 
@@ -28,5 +29,43 @@ class UnsettledPie(GroupedTransformer):
 
     def transform(self):
 
-        df = self.df
+        data = self.df[~self.df.bus_type.str.contains("贴现")]
+        # data['margin_ratio']  = data['margin_ratio'].fillna(0)
+        data['margin_ratio_c'] = 1 - data['margin_ratio'].fillna(0)
+        self.variables["debt_bal"] = np.dot(data.cur_bal.tolist(),
+                                            data.margin_ratio_c.tolist())
+        self.variables["debt_cnt"] = data.shape[0]
 
+        self.variables["inst_pie_name"]  = set(data.inst_name.tolist())
+        inst_pie_loan_bal = []
+        inst_pie_open_bal = []
+        for inst in self.variables["inst_pie_name"]:
+            inst_pie_loan_bal.append(
+                data[(data.inst_name == inst)
+                     & (pd.isnull(data.margin_ratio))].cur_bal.sum()
+            )
+
+            temp_df = data[(data.inst_name == inst)
+                        &(pd.notnull(data.margin_ratio))][['cur_bal','margin_ratio_c']]
+            inst_pie_open_bal.append(
+                np.dot(temp_df.cur_bal.tolist(),
+                       temp_df.margin_ratio_c.tolist())
+            )
+        self.variables["inst_pie_loan_bal"] = inst_pie_loan_bal
+        self.variables["inst_pie_open_bal"] = inst_pie_open_bal
+        self.variables["inst_pie_debt_prop"] = list(
+            (np.array(inst_pie_loan_bal) + np.array(inst_pie_open_bal)) / self.variables["debt_bal"]
+        )
+
+        self.variables["bus_pie_type"]  = set(data.bus_type.tolist())
+        bus_pie_debt_bal = []
+        for biz in self.variables["bus_pie_type"]:
+            temp_df = data[data.bus_type == biz][['cur_bal','margin_ratio_c']]
+            bus_pie_debt_bal.append(
+                np.dot(temp_df.cur_bal.tolist(),
+                       temp_df.margin_ratio_c.tolist())
+            )
+
+        self.variables["bus_pie_debt_prop"] = list(
+            np.array(bus_pie_debt_bal) / self.variables["debt_bal"]
+        )
