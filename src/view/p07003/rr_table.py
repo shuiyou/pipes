3503fcee6f342deedec6274a93926633449b9ee0
lishiquan@magfin.cn <lsq147LSQ>
2020-12-08 10:33:52 +0800

    12-8:  联调2.0  利息*一万;  未结清/对外担保 表格  按时间顺序

--- src/view/p07003/rr_table.py
+++ src/view/p07003/rr_table.py
@@ -97,7 +97,7 @@ class RrTable(GroupedTransformer):
 
         df = pd.concat([df,repay_duty1,repay_duty2] , ignore_index= True)
 
-        df = df.sort_values(by =['inst_name','due_date'] , ascending= [True,True])
+        df = df.sort_values(by =['due_date','inst_name'] , ascending= [True,True])
 
         df['start_date'] = df.start_date.apply(lambda x : str(x) if pd.notna(x) else None)
         df['due_date'] = df.due_date.apply(lambda x: str(x) if pd.notna(x) else None)
