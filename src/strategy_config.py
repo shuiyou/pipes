6bab66ae8b6ad46e0daa963986dc075416dd0122
lixiaobo <lixiaobo@magfin.cn>
2021-02-05 11:21:50 +0800

    feat: open strategy 切换 v2

--- src/strategy_config.py
+++ src/strategy_config.py
@@ -9,11 +9,11 @@ open_strategy_support_products = [
     "07002",  # 征信拦截
     "08001",  # 流水报告
     "08002",  # 流水拦截
-    "03002",  # 新版一级联合报告
     "07003",  # 企业征信报告
     "07004",  # 企业征信拦截
 ]
 
 
 def obtain_strategy_url(product_code=None):
-    return [STRATEGY_URL, OPEN_STRATEGY_URL][product_code and product_code in open_strategy_support_products]
+    return [STRATEGY_URL, OPEN_STRATEGY_URL][
+        product_code is not None and product_code in open_strategy_support_products]
