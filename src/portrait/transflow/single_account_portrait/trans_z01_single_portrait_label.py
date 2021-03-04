53cc25f3d8ca1323b8859ec5f43eba366db74d04
lishiquan <lishiquan@magfin.cn>
2021-03-04 11:37:34 +0800

    3-4: v1.0 , loan_type 标签调整3

--- src/portrait/transflow/single_account_portrait/trans_z01_single_portrait_label.py
+++ src/portrait/transflow/single_account_portrait/trans_z01_single_portrait_label.py
@@ -76,7 +76,8 @@ class TransSingleLabel:
         # 将字符串列合并到一起
         self.df['concat_str'] = self.df.apply(lambda x: ';'.join(x[concat_list]), axis=1)
         # 贷款类型赋值,优先级从上至下
-        self.df.loc[self.df['concat_str'].str.contains('消费金融|消费银企|汽车金融|陆金所|微粒贷|花呗|借呗|360还款|消费贷款|消金|京东金融|还呗|分期乐|平安普惠'),
+        self.df.loc[(self.df['concat_str'].str.contains('消费金融|消费银企|汽车金融|陆金所|微粒贷|花呗|借呗|360还款|消费贷款|消金|京东金融|还呗|分期乐|平安普惠')) |
+                    ( (self.df['concat_str'].str.contains("美团")) & (self.df['concat_str'].str.contains("放款"))) ,
                     'loan_type'] = '消金'
         # self.df.loc[(self.df['concat_str'].str.contains('银行贷款|银行.*放款|放款.*银行')) &
         #             (pd.isnull(self.df.loan_type)), 'loan_type'] = '银行'
@@ -88,26 +89,28 @@ class TransSingleLabel:
         self.df.loc[(self.df['concat_str'].str.contains('保理')) &
                     (pd.isnull(self.df.loan_type)), 'loan_type'] = '保理'
         self.df.loc[~(self.df['concat_str'].str.contains('小额贷记来账|信用卡')) &
-                    (self.df['concat_str'].str.contains('小额贷|小贷|典当|互联网信息咨询|360借条|中安信业|360网络贷款|陕银消费')) &
+                    (self.df['concat_str'].str.contains('小额贷|小贷|典当|互联网信息咨询|360借条|中安信业|360网络贷款|陕银消费|瀚华|富登')) &
                     (pd.isnull(self.df.loan_type)), 'loan_type'] = '小贷'
 
-        self.df.loc[((self.df['concat_str'].str.contains('银行贷款|银行.*放款|贷款放款|对公贷款|贷款利息收入|放款过渡户|信贷业务|受托支付|企业贷|个贷')) |
+        self.df.loc[((self.df['concat_str'].str.contains('银行贷款|银行.*放款|贷款放款|对公贷款|贷款利息收入|放款过渡户|信贷业务|受托支付|企业贷|个贷|网商贷')) |
                      ((pd.isnull(self.df.opponent_type)) & (
                          self.df['concat_str'].str.contains('批量代扣|批量扣款|自动收利|约定还款|批扣|贷款扣款|批量还款|自动还款|信贷放款|收回利息|贷款业务|放款|还息|还贷')))) &
-                    (~self.df['concat_str'].str.contains('信用卡|收单|短信服务|工资|电费|缴税|手续费|回单服务|人寿保险|管理费|服务费|信使费|ETC|POS')) &
+                    (~self.df['concat_str'].str.contains('信用卡|收单|短信.*服务|工资|电费|缴税|手续费|回单服务|人寿保险|管理费|服务费|信使费|ETC|POS')) &
                     (pd.isnull(self.df.loan_type)),
                     'loan_type'] = '银行'
-        self.df.loc[(self.df['concat_str'].str.contains('支付宝|特约|聚合支付|平安付'))
+        self.df.loc[(self.df['concat_str'].str.contains('支付宝|特约|聚合支付|平安付|网银在线'))
                     & (self.df['concat_str'].str.contains('借|还'))
                     & (pd.isnull(self.df.loan_type)
                        & (~self.df['concat_str'].str.contains('信用卡|银行'))),
                     'loan_type'] = '第三方支付'
         self.df.loc[
-            (((self.df['concat_str'].str.contains(
-                '信托|信贷.*过渡户|过渡户.*信贷|财务.*公司|资金互助社|金融.*公司|供应链融资|车贷放款|融资作业|租赁.*还|其他外围|陆家嘴国际金融资产交易市场')) &
-              (~self.df['concat_str'].str.contains('综合金融服务平台|财务咨询|小微商户|中移动金融科技|金融城|工程款|材料款|预付款|保证金|项目款|代理费|竞价|报价|运输款|运输费'))) | (
-                         (self.df['concat_str'].str.contains('车贷|还车款|车辆按揭|汽车金融')) &
-                         (self.df.opponent_type != 1)))
+            (
+                ((self.df['concat_str'].str.contains(
+                '信托|信贷.*过渡户|过渡户.*信贷|财务.*公司|资金互助社|金融.*公司|供应链融资|车贷放款|融资作业|租赁.*还|其他外围|陆家嘴国际金融资产交易市场|保险.*贷款')) &
+              (~self.df['concat_str'].str.contains('综合金融服务平台|财务咨询|小微商户|中移动金融科技|金融城|工程款|材料款|预付款|保证金|项目款|代理费|竞价|报价|运输款|运输费'))) |
+             (  (self.df['concat_str'].str.contains('车贷|还车款|汽车金融')) & (self.df.opponent_type != 1)  )  |
+              ( (self.df['opponent_name'].str.contains('|'.join(self.relation_dict.keys())))&(self.df['concat_str'].str.contains("车辆按揭")) )
+            )
             &(~self.df['trans_channel'].str.contains("清算中心"))
             & (pd.isnull(self.df.loan_type)), 'loan_type'] = '其他金融'
 
